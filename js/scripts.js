5a0ff85340995d6256581da397e8ae1c7d9f382a
Michael Watts <wattsjmichael@gmail.com>
2020-08-28 08:27:27 -0700

    create Pizza Object

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
+function Pizza(size, meat, veggie, addon) {
+  this.size = size;
+  this.meat = meat;
+  this.veggie = veggie;
+  this.addon = addon;
+  this.price = "";
+}
+
+
+
+
+
 $(document).ready(function(){
     $("#formOne").submit(function(event){
         event.preventDefault();
