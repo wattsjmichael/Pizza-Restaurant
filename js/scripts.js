eeb9f3c9bea6434e4be523520c681d5552008889
Michael Watts <wattsjmichael@gmail.com>
2020-08-28 13:08:14 -0700

    logic for pizza ordering is showing the correct value, not 10000

--- js/scripts.js
+++ js/scripts.js
@@ -7,8 +7,10 @@ function Pizza(size, meat, vegetable) {
 }
 
 Pizza.prototype.pizzaPrice = function() {
-  return this.size + (this.meat*2) + (this.vegetable/2);
+  return this.size + this.meat + this.vegetable;
 }
+
+
 
 
 
@@ -16,8 +18,8 @@ $(document).ready(function(){
     $("form#pizzaOrder").submit(function(event){
         event.preventDefault();
 
-
         let size = parseInt($("input:radio[name=size]:checked").val());
+        
         //Meat Logic
         var meats = [];
         $("#meatToppings input:checked").each(function (i){
@@ -25,6 +27,7 @@ $(document).ready(function(){
         });
         let meatsTotal = 0;
         $.each(meats, function(){meatsTotal+=parseInt(this) || 0;});
+        
         //Vegetable Logic
         var vegetables= [];
         $("#vegetableToppings input:checked").each(function(i){
@@ -34,7 +37,6 @@ $(document).ready(function(){
         $.each(vegetables, function(){vegetablesTotal+=parseInt(this) || 0;})
         //Pizza Logic
         let newPizza = new Pizza(size, meatsTotal, vegetablesTotal);
-        console.log(size);
         $("#result").show();
         $("#order").text(newPizza.pizzaPrice());
     });
